a6e147cdaa9fcbe79b55aeb40ccf5e55f3b8e82a
Tanbir Haque <tanbirhaque53@gmail.com>
2024-05-31 10:47:54 +0600

    Card design and currency symbol changed

--- src/components/shared/ProductCard/ProductCard.tsx
+++ src/components/shared/ProductCard/ProductCard.tsx
@@ -8,6 +8,9 @@ import Modal from "./Modal";
 import { PiEyeThin, PiHeartStraightThin, PiShareFatThin, PiShareNetworkThin } from "react-icons/pi";
 import { MdShare } from "react-icons/md";
 import { HiShare } from "react-icons/hi2";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
+import "./CardRating.css"
 
 // interface ProductCardProps {
 //     product: Product;
@@ -102,71 +105,35 @@ const ProductCard = ({ product }: any) => {
           </div>
           <Link href={`/${product?._id}`}>
             <div>
-              <h3 className="font-jost font-bold text-xl mt-7 ml-1 line-clamp-1">
+              {/* Ratings */}
+              <div className="card-Rating mt-5" >
+                <Rating
+                  readOnly
+                  style={{ maxWidth: 120 }}
+                  value={3}
+                // onChange={onChange}
+                />
+              </div>
+              <h3 className="font-jost font-bold text-xl mt-2 mb-1 ml-1 line-clamp-1">
                 {product.title}
               </h3>
-              <div className="flex justify-between items-end">
+              {/* <h3 className="font-jost font-bold text-xl mt-7 ml-1 mb-3 line-clamp-1">
+                {product.title}
+              </h3> */}
+              <div className="flex justify-between items-center">
                 <div>
-                  {/* Ratings */}
-                  <div className="flex items-center my-2">
-                    <svg
-                      className="w-4 h-4 text-yellow-300 ms-1"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                      className="w-4 h-4 text-yellow-300 ms-1"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                      className="w-4 h-4 text-yellow-300 ms-1"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                      className="w-4 h-4 text-yellow-300 ms-1"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                      className="w-4 h-4 ms-1 text-gray-300 dark:text-gray-500"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                  </div>
                   {/* Price */}
                   <div className="ml-1">
                     {discount ? (
-                      <div className="flex gap-2 items-center">
-                        <del>${price}</del>
-                        <p className="text-[20px] font-bold text-[#fa8c16]">
-                          ${result.discountedPrice.toFixed(2)}
+                      <div className="flex flex-row-reverse gap-2 items-center">
+                        <del className="text-gray-500">&#x9F3;{price}</del>
+                        <p className="text-[22px] font-bold text-[#fa8c16]">
+                          &#x9F3;{result.discountedPrice.toFixed(2)}
                         </p>
                       </div>
                     ) : (
-                      <p className="text-[20px] font-bold text-[#fa8c16]">
-                        ${price}
+                      <p className="text-[22px] font-bold text-[#fa8c16]">
+                        &#x9F3;{price}
                       </p>
                     )}
                   </div>
